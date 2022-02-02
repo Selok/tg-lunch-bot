ea9c1d374364fde161d1b93f2da0c9d84b610201
Selok <selokyeung@gmail.com>
2022-02-03 07:43:32 +0800

    update: scheduler command

--- decide_lunch/scheduler.py
+++ decide_lunch/scheduler.py
@@ -1,10 +1,14 @@
+import os
+from turtle import up
+import uuid
 import asyncio
-import aiofile as aiof
 import json
-from typing import List
-from datetime import datetime
+from typing import Any, Dict, Set, List, NamedTuple
+from datetime import datetime, timedelta
 
+import aiofile as aiof
 
+from config import ENROLL_DIR
 from utils.base import LoggingBase
 from message.base import Messager
 from enum import Enum
@@ -21,6 +25,15 @@ class Action(Enum):
 class ScheduleType(Enum):
     Weekday: 1
     OneTime: 2
+    Daily: 3
+
+
+class Schedule(NamedTuple):
+    chat_id: int
+    lunch_id: str
+    title: str
+    time: datetime
+    type: ScheduleType
 
 
 class Scheduler(LoggingBase):
@@ -36,23 +49,64 @@ class Scheduler(LoggingBase):
     __slots__ = [
         '_messager',
         '_enabledChats',
-        '_running'
+        '_running',
+        '_notified'
     ]
 
     def __init__(self, enabledChats: List[int], messager: Messager):
         self._messager = messager
-        self._enabledChats = enabledChats
+        self._enabledChats: Dict[int, List[Schedule]] = {
+            chat_id: [] for chat_id in enabledChats}
+        self._notified: Dict[int, Set[str]] = {
+            chat_id: [] for chat_id in enabledChats}
         super(Scheduler, self).__init__()
 
     def setup(self):
         """initial object
         """
         self._running = False
+        for chat_id in self._enabledChats:
+            json_path = os.path.join(
+                ENROLL_DIR,
+                f"{chat_id}.json"
+            )
+            with open(json_path) as f:
+                self._enabledChats[chat_id] = self._parseSchedule(json.load(f))
+
+        self._messager.addConsumer(self.parseMsg)
 
     def cleanup(self):
         return super().cleanup()
 
-    async def parseSchedule(self, msg: str):
+    def _parseSchedule(self, jsons: Any) -> List[Schedule]:
+        try:
+            schedules = []
+
+            chat_id = jsons['chat_id']
+            for sch in jsons['schedules']:
+                lunch_id = sch['lunch_id']
+                title = sch['title']
+                time = datetime.strptime(sch['time'], '%Y%m%d%H%M')
+                schType = ScheduleType(int(sch['schType']))
+                schedules.append(Schedule(
+                    chat_id, lunch_id, title,
+                    time, schType
+                ))
+
+            return schedules
+        except:
+            self.log.error("Invalid json file")
+
+    async def _updateJson(self, chatId: str):
+        json_path = os.path.join(
+            ENROLL_DIR,
+            f"{chatId}.json"
+        )
+        json_str = json.dump(self._enabledChats[chatId])
+        async with aiof.async_open(json_path, 'w') as afp:
+            await afp.write(json_str)
+
+    async def parseMsg(self, msg: str):
         try:
             action: Action
             with json.loads(msg) as j:
@@ -84,30 +138,139 @@ class Scheduler(LoggingBase):
         except:
             self.log.warning("Invalid message")
 
-    async def setupLunch(self, chatId: str, title: str, schType: ScheduleType, datetime: datetime):
-        pass
+    async def setupLunch(self, chatId: str, title: str, schType: ScheduleType, time: datetime):
+        try:
+            sch = Schedule(
+                chatId, str(uuid.uuid4()), title,
+                time, schType
+            )
+            self._enabledChats[chatId].append(sch)
+            await self._updateJson(chatId)
+            feedback = {
+                'chatId': chatId,
+                'action': int(Action.Setup),
+                'success': True,
+                'lunch_id': sch.lunch_id,
+                'title': sch.title
+            }
+        except:
+            self.log.error("Setup lunch fail")
+            feedback = {
+                'chatId': chatId,
+                'action': int(Action.Setup),
+                'success': False
+            }
 
-    async def modifyLunch(self, chatId: str, lunch_id: str, title: str, schType: ScheduleType, datetime: datetime):
-        pass
+        await self._messager.send(json.dump(feedback))
+
+    async def modifyLunch(self, chatId: str, lunch_id: str, title: str, schType: ScheduleType, time: datetime):
+        success = False
+        try:
+            for sch in self._enabledChats[chatId]:
+                if sch.lunch_id == lunch_id:
+                    sch.title = title
+                    sch.type = schType
+                    sch.time = time
+                    await self._updateJson(chatId)
+                    success = True
+                    break
+        except:
+            self.log.error("Modify lunch fail")
+
+        feedback = {
+            'chatId': chatId,
+            'action': int(Action.Modify),
+            'success': success,
+            'lunch_id': lunch_id,
+            'title': title
+        }
+
+        await self._messager.send(json.dump(feedback))
 
     async def skipLunch(self, chatId: str, lunch_id: str):
-        pass
+        success = False
+        try:
+            self._notified[chatId].add(lunch_id)
+            success = True
+        except:
+            self.log.error("Modify lunch fail")
+
+        feedback = {
+            'chatId': chatId,
+            'action': int(Action.Skip),
+            'success': success,
+            'lunch_id': lunch_id
+        }
+        await self._messager.send(json.dump(feedback))
 
     async def cancelLunch(self, chatId: str, lunch_id: str):
-        pass
+        success = False
+        try:
+            for i, sch in enumerate(self._enabledChats[chatId]):
+                if sch.lunch_id == lunch_id:
+                    del self._enabledChats[chatId][i]
+                    await self._updateJson(chatId)
+                    success = True
+                    break
+        except:
+            self.log.error("Cancel lunch fail")
+
+        feedback = {
+            'chatId': chatId,
+            'action': int(Action.Cancel),
+            'success': success,
+            'lunch_id': lunch_id
+        }
+
+        await self._messager.send(json.dump(feedback))
 
     async def listLunch(self, chatId: str):
-        pass
+        feedback = {
+            'chatId': chatId,
+            'action': int(Action.List),
+            'schedules': self._enabledChats[chatId]
+        }
 
-    async def notifyLunch(self, chatId: str):
-        pass
+        await self._messager.send(json.dump(feedback))
+
+    async def notifyLunch(self):
+        now = datetime.now()
+
+        for chatId in self._enabledChats:
+            schedules = self._enabledChats[chatId]
+            notified = self._notified[chatId]
+            need_cleanup = []
+
+            for sch in schedules:
+                if sch.type in [ScheduleType.Daily, ScheduleType.Weekday]:
+                    sch_time = sch.time.replace(
+                        year=now.year, month=now.month, day=now.day
+                    )
+                else:
+                    sch_time = sch.time
+                if sch.lunch_id in self._notified[chatId] and now > sch_time:
+                    notified.remove(sch.lunch_id)
+                elif now < sch_time and sch_time - now < timedelta(minutes=30):
+                    feedback = {
+                        'chatId': chatId,
+                        'schedule': sch
+                    }
+                    await self._messager.send(json.dump(feedback))
+
+                    if sch.type == ScheduleType.OneTime:
+                        need_cleanup.append(sch)
+
+            for sch in need_cleanup:
+                schedules.remove(sch)
+
+            if len(need_cleanup) > 0:
+                await self._updateJson(chatId)
 
     async def start(self):
         self._running = True
         while self._running:
+            await self.notifyLunch()
             await asyncio.sleep(1)
-            for chatId in self._enabledChats:
-                await self.notifyLunch(chatId)
 
     def stop(self):
         self._running = False
